3479f8c01fb5be3aecfbaea3076ec6951a1f0170
Jos3Real <josedarealg@gmail.com>
2026-03-12 19:56:36 -0600

    Se agrega el documento con los tipos de datos más usados en C++

--- TiposDatos.cpp
+++ TiposDatos.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main () {
 
-    int entero = 5;
+    int num_entero = 5;
     float flotante = 15.26;
     double num_double = 16.254857;
     char letra = 'a';
 
-    cout << entero << endl;
+    cout << num_entero << endl;
     cout << flotante << endl;
     cout << num_double << endl;
     cout << letra;
