1258ac0f3dbed01d77b98b33b6058c718bfbfe5a
Jos3Real <josedarealg@gmail.com>
2026-03-12 19:51:04 -0600

    Se agrega el documento con los tipos de datos básicos usados en C++

---
+++ TiposDatos.cpp
@@ -0,0 +1,19 @@
+// Tipos de datos básicos en C++
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int entero = 5;
+    float flotante = 15.26;
+    double num_double = 16.254857;
+    char letra = 'a';
+
+    cout << entero << endl;
+    cout << flotante << endl;
+    cout << num_double << endl;
+    cout << letra;
+
+    return 0;
+}
